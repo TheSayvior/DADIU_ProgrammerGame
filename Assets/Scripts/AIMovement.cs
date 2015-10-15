c904a30b0add94138a81e4a584c4762bd2e1349a
AAwezz <anders_wess@hotmail.com>
2015-10-15 20:33:42 +0200

    AI movement done, and start enemies set

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class AIMovement : MonoBehaviour {
 
-    public Transform destination;
+    //public Transform destination;
+    GameObject player;
     private NavMeshAgent agent;
 
     void Start () 
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        agent.SetDestination(destination.position);
+        agent.SetDestination(player.transform.position);
     }
 }
 
---
+++ Assets/Scripts/SpawnAI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAI : MonoBehaviour {
+    public GameObject AI;
+    public int startEnemies;
+    int x, z, numberOfAIsStart = 0;
+
+	// Use this for initialization
+	void Start () {
+        while (numberOfAIsStart != startEnemies)
+        {
+            StartCoroutine("SpawnOneAICou");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void SpawnOneAI()
+    {
+        x = Random.Range(-45, 45);
+        z = Random.Range(25, -65);
+        if (x > 15 || x < -15 && z > 15 || z < -15)
+        {
+            AI.transform.position = new Vector3(x, 1, z);
+            Instantiate(AI);
+            numberOfAIsStart++;
+        }
+    }
+
+    IEnumerator SpawnOneAICou()
+    {
+        SpawnOneAI();
+        yield return null;
+    }
+}
