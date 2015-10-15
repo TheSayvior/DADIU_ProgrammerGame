f277012a0414b65083265abee0c213ef75dde9db
AAwezz <anders_wess@hotmail.com>
2015-10-15 13:57:02 +0200

    Level and timer done

---
+++ DADIU programmer project/Assets/CountdownTimer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CountdownTimer : MonoBehaviour {
+
+    public Text timerTxt;
+    float timer = 0, timelimit = 180;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimerCountdown();
+	}
+
+    void TimerCountdown()
+    {
+        timelimit -= Time.deltaTime;
+        timerTxt.text = "Time left: " + timelimit.ToString("f2");
+    }
+}
