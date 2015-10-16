79acc037ac429138ee337c4a0923b7fb3e842c0b
MrZenji <T2700B@hotmail.com>
2015-10-16 14:55:19 +0200

    Player can shoot

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public int damagePerShot = 20;
+    Vector3 direction;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            transform.position = transform.position + direction;
+        }
+    }
+
+    void setDirection(Vector3 dir)
+    {
+        direction = dir;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "AI")
+        {
+            EnemyHealth enemyHealth = col.gameObject.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damagePerShot, Vector3.zero);
+                //Destroy(gameObject);
+            }
+        }
+        else if (col.gameObject.tag =="Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
