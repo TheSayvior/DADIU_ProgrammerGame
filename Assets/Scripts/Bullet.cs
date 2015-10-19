39f16d21996b1c2f2af54a33d9887249143360fc
MrZenji <T2700B@hotmail.com>
2015-10-19 09:33:42 +0200

    Added shooting again

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+    public int damagePerShot;
+    Vector3 direction;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            transform.position = transform.position + direction;
+        }
+    }
+
+    void setDirection(Vector3 dir)
+    {
+        direction = dir;
+    }
+
+    void setDmg(int dmg)
+    {
+        damagePerShot = dmg;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "AI")
+        {
+            EnemyHealth enemyHealth = col.gameObject.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damagePerShot, Vector3.zero);
+                Destroy(gameObject);
+            }
+        }
+        else if (col.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float ReloadSpeed = 2f;
+    public GameObject bullet, shootingLine;
+
+
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets)
+        {
+            Shoot();
+        }
+    }
+
+
+    void Shoot()
+    {
+        timer = 0f;
+        GameObject bul = Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        bul.SendMessage("setDirection", (transform.position - shootingLine.transform.position) * ReloadSpeed);
+        bul.SendMessage("setDmg", damagePerShot);
+    }
+}
